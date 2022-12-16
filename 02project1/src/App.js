42a0570c66c71c4be34a990aa435ebaab66c2146
Sadlark1993 <claudio.zi.junior@gmail.com>
2022-12-16 12:11:30 -0400

    Lesson: arrays and objects in states

--- 02project1/src/App.js
+++ 02project1/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
@@ -8,47 +7,38 @@ class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-      name: 'Cláudio',
-      lastName: 'Zimmermann',
-      counter: 1,
+      posts: [
+        {
+          id: 1,
+          title: 'Title 1',
+          body: 'body 1'
+        },
+        {
+          id: 2,
+          title: 'Title 2',
+          body: 'body 2'
+        },
+        {
+          id: 3,
+          title: 'Title 3',
+          body: 'body 3'
+        },
+      ]
     }
-
-    //bind
-    this.handlePClick = this.handlePClick.bind(this);
   }
 
-  handlePClick(){
-    //every time the state changes, the render() method is called again.
-    this.setState({name: 'Junior'});
-  }
-
-  //arrow functions does't have 'this', so it searches for it's parent's this. So, does't need to do the bind.
-  handleAClick = (event) => {
-    event.preventDefault();
-    const {counter} = this.state;
-    this.setState({counter: counter+1});
-
-  }
 
   render(){
-    const {name, counter} = this.state;
+    const {posts} = this.state;
     return( //needs to return a JSX
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick = {this.handlePClick}> {/* added a event listener onClick */}
-            Hello {name} {counter}!! {/* using var */}
-          </p>
-          <a
-            onClick={this.handleAClick}
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learning React
-          </a>
-        </header>
+        {posts.map(post=>( //returning this whole parenthesis.
+          //always put a key in the first element of the .map().
+          <div key={post.id}>
+            <h1 >{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
       </div>
     );
   }
