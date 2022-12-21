54a5274860474824945be4c945ff98045763a6b3
Sadlark1993 <claudio.zi.junior@gmail.com>
2022-12-21 14:47:25 -0400

    Lesson: useContext() - part 2: using with useState()

--- 03hooks/src/App.js
+++ 03hooks/src/App.js
@@ -1,34 +1,44 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import './App.css';
 
 const stateObj = {
   title: 'Title of the Global State',
   body: 'The word "body" in portuguese means goat.',
+  counter: 0,
 };
 
-/*to avoid to have to being passing props between several components to just serve the last one,
-we use Context.*/
+//Creating the context...
 const GlobalContext = React.createContext();
 
 const Div = () => {
   return <H1 />;
 };
 
+//With context, this component does't need to receive props to access the state. It can also alter the state.
 const H1 = () => {
-  const theContext = useContext(GlobalContext); //importing the object
+  const { globalState, setGlobalState } = useContext(GlobalContext); //importing the object
+  //console.log(globalState);
+
+  //careful when using setGlobalState(). It changes the state, and can trigger a infinity loop of rendering.
   return (
     <div>
-      <h1>{theContext.title}</h1>
-      <p>{theContext.body}</p>
+      <h1 style={{ cursor: 'pointer' }} onClick={() => setGlobalState((obj) => ({ ...obj, counter: obj.counter + 1 }))}>
+        {globalState.title}
+      </h1>
+      <p>
+        {globalState.body} Counter: {globalState.counter}
+      </p>
     </div>
   );
 };
 
 function App() {
+  const [globalState, setGlobalState] = useState(stateObj);
+
   return (
     <div className="App">
-      {/* every child of this component can access this obj now */}
-      <GlobalContext.Provider value={stateObj}>
+      {/* The child components now can alter the state using the function setGlobalState() */}
+      <GlobalContext.Provider value={{ globalState, setGlobalState }}>
         <Div />
       </GlobalContext.Provider>
     </div>
