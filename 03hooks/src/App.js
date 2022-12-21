b2ca73c4174ff5b58e82dfd8f4ac84b5785f6e84
Sadlark1993 <claudio.zi.junior@gmail.com>
2022-12-20 21:33:42 -0400

    Lesson: useMemo() - Part 1

--- 03hooks/src/App.js
+++ 03hooks/src/App.js
@@ -2,18 +2,13 @@
 
 import P from 'prop-types';
 import './App.css';
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useMemo } from 'react';
 
 //always remember to do the destructuring...
-/*
-with the 'incrementFn' being inside a useCallback(), his value will not change when the state changes,
-so, the argument of Button() will not change as well. It makes memoized Button to not be rendered every
-time the state change.
-*/
-const Button = React.memo(function Button({ incrementFn }) {
-  console.log('child ron');
+const Button = ({ incrementFn }) => {
+  console.log('child ran');
   return <button onClick={() => incrementFn(10)}>+</button>;
-});
+};
 
 Button.propTypes = {
   incrementFn: P.func,
@@ -27,17 +22,30 @@ function App() {
   //states: every time the state changes, the component is re-tenderized on screen.
   const [counter, setCounter] = useState(0);
 
+  /*
+  with the 'incrementFn' being inside a useCallback(), his value will not change when the state changes,
+  so, the argument of Button() will not change as well. It makes memoized Button to not be rendered every
+  time the state change.
+  */
   const incrementFn = useCallback((num) => {
     setCounter((prevCounter) => prevCounter + num);
     console.log('incremented');
   }, []);
 
-  console.log('Parent ron');
+  console.log('Parent ran');
 
   return (
     <div className="App">
       <h1>Contador: {counter}</h1>
-      <Button incrementFn={incrementFn} />
+      {/* useMemo() is the same thing as useCallback(), but it returns a value instead a function.
+        If the content of the dependencies array don't change, what is inside useMemo() will not
+        re-render. */}
+      {useMemo(
+        () => (
+          <Button incrementFn={incrementFn} />
+        ),
+        [incrementFn],
+      )}
     </div>
   );
 }
