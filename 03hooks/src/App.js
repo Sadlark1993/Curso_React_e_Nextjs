5e64bee5e6e5abeee1ca3d33bb9c9e4a896037a9
Sadlark1993 <claudio.zi.junior@gmail.com>
2022-12-21 18:25:59 -0400

    Lesson: useReducer()

--- 03hooks/src/App.js
+++ 03hooks/src/App.js
@@ -1,13 +1,39 @@
+//use reducer is used with complex states.
+import { useReducer } from 'react';
 import './App.css';
-import { AppContext } from './contexts/AppContext';
-import { Div } from './components/Div';
+
+const initialState = {
+  title: 'This is the Title of the Body',
+  body: 'Goat in portuguese is "bode".',
+  counter: 0,
+};
+
+//function reducer(state, action) will be called when the dispatch function is executed.
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'change': {
+      return { ...state, title: 'The Title has Changed: ' + action.payload };
+    }
+    //if you are going to declare any variable inside a switch-case, wrap the case with braces '{}'
+    case 'invert': {
+      const { title } = state;
+      return { ...state, title: title.split('').reverse().join('') };
+    }
+  }
+  return { ...state };
+};
 
 function App() {
+  /*the dispatch(action) function will call the reducer(state, action) function. The return of the
+    reduce function is the new state.*/
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div className="App">
-      <AppContext>
-        <Div />
-      </AppContext>
+      <h1>{state.title}</h1>
+      <p>{state.body}</p>
+      <button onClick={() => dispatch({ type: 'invert' })}>Invert</button>
+      <button onClick={() => dispatch({ type: 'change', payload: new Date().toLocaleString('pt-BR') })}>Change</button>
     </div>
   );
 }
--- 03hooks/src/contexts/AppContext/Data.js
+++
@@ -1,5 +0,0 @@
-export const stateObj = {
-  title: 'Title of the Global State',
-  body: 'The word "body" in portuguese means goat.',
-  counter: 0,
-};
