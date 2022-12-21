3e99d2355f0a8586be42f127f7810cfe4601cfdb
Sadlark1993 <claudio.zi.junior@gmail.com>
2022-12-21 19:57:18 -0400

    Lesson: useContext() + useReduce()

--- 03hooks/src/App.js
+++ 03hooks/src/App.js
@@ -1,39 +1,83 @@
 //use reducer is used with complex states.
-import { useReducer } from 'react';
+
+import P from 'prop-types';
+import { createContext, useContext, useReducer, useRef } from 'react';
 import './App.css';
 
-const initialState = {
+//AppData.js
+export const initialState = {
   title: 'This is the Title of the Body',
-  body: 'Goat in portuguese is "bode".',
+  body: 'This is a perfect and beautiful body',
   counter: 0,
 };
 
-//function reducer(state, action) will be called when the dispatch function is executed.
-const reducer = (state, action) => {
+//reducer.js
+export const reducer = (state, action) => {
   switch (action.type) {
-    case 'change': {
-      return { ...state, title: 'The Title has Changed: ' + action.payload };
-    }
-    //if you are going to declare any variable inside a switch-case, wrap the case with braces '{}'
-    case 'invert': {
-      const { title } = state;
-      return { ...state, title: title.split('').reverse().join('') };
+    case 'ChangeTitle': {
+      return { ...state, title: action.payload };
     }
   }
   return { ...state };
 };
 
-function App() {
-  /*the dispatch(action) function will call the reducer(state, action) function. The return of the
-    reduce function is the new state.*/
-  const [state, dispatch] = useReducer(reducer, initialState);
+//AppContext.js
+export const Context = createContext();
 
+export const AppContext = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  console.log(dispatch);
+
+  //encapsulate the dispatch function.
+  const changeTitle = (payload) => {
+    dispatch({ type: 'ChangeTitle', payload: payload });
+  };
+
+  //always remember to destructure the object of the value.
+  return <Context.Provider value={{ state, changeTitle }}>{children}</Context.Provider>;
+};
+
+AppContext.propTypes = {
+  children: P.node.isRequired,
+};
+
+//H1.jsx
+export const H1 = () => {
+  const context = useContext(Context);
+  //console.log(context.state);
+  return <h1>{context.state.title}</h1>;
+};
+
+//Parag.jsx
+export const Parag = () => {
+  const context = useContext(Context);
+  return <p>{context.state.body}</p>;
+};
+
+//Btn.jsx
+export const Btn = () => {
+  const input = useRef(null); //initiates this referencing null.
+  const context = useContext(Context);
+  return (
+    <>
+      <button onClick={() => context.changeTitle(input.current.value)}>Change Title</button>
+      <p>
+        <input ref={input} />
+      </p>
+    </>
+  );
+};
+
+function App() {
   return (
     <div className="App">
-      <h1>{state.title}</h1>
-      <p>{state.body}</p>
-      <button onClick={() => dispatch({ type: 'invert' })}>Invert</button>
-      <button onClick={() => dispatch({ type: 'change', payload: new Date().toLocaleString('pt-BR') })}>Change</button>
+      <AppContext>
+        <div>
+          <H1 />
+          <Parag />
+          <Btn />
+        </div>
+      </AppContext>
     </div>
   );
 }
