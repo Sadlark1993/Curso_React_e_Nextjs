3c82fe91ea8da3fad5cd5f158468126bb1aa06b5
Sadlark1993 <claudio.zi.junior@gmail.com>
2022-12-23 14:06:54 -0400

    Lesson: third custom hook

--- 03hooks/src/App.jsx
+++ 03hooks/src/App.jsx
@@ -1,101 +1,63 @@
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useState, shouldRun } from 'react';
 import './style.css';
 
-const isObjectEqual = (objA, objB) => {
-  return JSON.stringify(objA) === JSON.stringify(objB);
+const useAsyncFunction = (asyncFunction, shouldRun) => {
+  const [result, setResult] = useState({
+    result: null,
+    error: null,
+    status: 'idle',
+  });
+
+  const run = useCallback(() => {
+    setResult({
+      result: null,
+      error: null,
+      status: 'pending',
+    });
+
+    return asyncFunction()
+      .then((response) => {
+        setResult({ result: response, error: null, status: 'settled' });
+      })
+      .catch((error) => {
+        setResult({ result: null, error: error, status: 'error' });
+      });
+  }, [asyncFunction]);
+
+  useEffect(() => {
+    if (shouldRun) {
+      run();
+    }
+  }, [shouldRun, run]);
+
+  return [run, result.result, result.error, result.status];
 };
 
-const useFetch = (url, options) => {
-  const [result, setResult] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [shouldLoad, setShouldLoad] = useState(false);
-  const urlRef = useRef(url);
-  const optionsRef = useRef(options);
-
-  useEffect(() => {
-    let changed = false;
-    if (!isObjectEqual(urlRef.current, url)) {
-      urlRef.current = url;
-      changed = true;
-    }
-
-    if (!isObjectEqual(optionsRef.current, options)) {
-      optionsRef.current = options;
-      changed = true;
-    }
-
-    if (changed) setShouldLoad((s) => !s);
-  }, [url, options]);
-
-  useEffect(() => {
-    //the var wait is to the states don't change when the component is already unmounted
-    let wait = false;
-    setLoading(true);
-    const controller = new AbortController();
-    const signal = controller.signal;
-
-    const fetchData = async () => {
-      await new Promise((r) => setTimeout(r, 1000));
-
-      try {
-        const response = await fetch(urlRef.current, { signal, ...optionsRef.current });
-        const jsonResult = await response.json();
-
-        if (!wait) {
-          setResult(jsonResult);
-          setLoading(false);
-        }
-      } catch (e) {
-        if (!wait) setLoading(false);
-        console.warn('Error: ', e.message);
-      }
-    };
-
-    fetchData();
-
-    return () => {
-      wait = true;
-      controller.abort();
-    };
-  }, [shouldLoad]);
-
-  return [result, loading];
+const getData = async () => {
+  const data = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const dataObj = await data.json();
+  return dataObj;
 };
 
 export function App() {
-  const [postId, setPostId] = useState('');
-  //if I uncomment the {method: 'GET'}, the useFetch will execute several times causing error.
-  //teacher solved problem using another hook again (useRef).
-  const [result, loading] = useFetch('https://jsonplaceholder.typicode.com/posts/' + postId, { headers: { abc: 1 } });
+  const [refetch, result, error, status] = useAsyncFunction(getData, true);
 
-  const handleClick = (id) => {
-    setPostId(id);
-  };
+  //disabled eslint no-unused-vars. To it does't keep yelling at me for not be using refetch.
 
-  if (loading) {
-    return <h1>Loading...</h1>;
-  }
-  if (!loading) {
+  if (status === 'idle') {
+    return <h3>Ok, nothing to do here...</h3>;
+  } else if (status === 'pending') {
+    return <h3>Loading...</h3>;
+  } else if (status === 'settled') {
+    return <pre>hallo: {JSON.stringify(result, null, 2)}</pre>;
+  } else if (status === 'error') {
     return (
-      <div>
-        <h1>Title</h1>
-        {/* here, he used a ternary operator to chose if should print an array or an object. If you click
-        in a title, it will load the object. If we click again, it will load the hole array again. */}
-        {result?.length > 0 ? (
-          result.map((post) => {
-            return (
-              <div key={post.id}>
-                <h3 onClick={() => handleClick(post.id)}>{post.title}</h3>
-              </div>
-            );
-          })
-        ) : (
-          <div key={result.id}>
-            <h3 onClick={() => handleClick('')}>{result.title}</h3>
-          </div>
-        )}
-      </div>
+      <>
+        <h3>This is a disaster!!!</h3> <p>{error}</p>
+      </>
     );
+  } else {
+    return <h3> I will quit this job. Excuse-me...</h3>;
   }
 }
 
