34e9c1954b90dd371e6c6030c8be912bae95648a
Sadlark1993 <claudio.zi.junior@gmail.com>
2022-12-23 11:27:40 -0400

    Lesson: added an effect on the titles, fixed the url and options comparison.

--- 03hooks/src/App.jsx
+++ 03hooks/src/App.jsx
@@ -1,57 +1,96 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './style.css';
+
+const isObjectEqual = (objA, objB) => {
+  return JSON.stringify(objA) === JSON.stringify(objB);
+};
 
 const useFetch = (url, options) => {
   const [result, setResult] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [shouldLoad, setShouldLoad] = useState(false);
+  const urlRef = useRef(url);
+  const optionsRef = useRef(options);
 
   useEffect(() => {
+    let changed = false;
+    if (!isObjectEqual(urlRef.current, url)) {
+      urlRef.current = url;
+      changed = true;
+    }
+
+    if (!isObjectEqual(optionsRef.current, options)) {
+      optionsRef.current = options;
+      changed = true;
+    }
+
+    if (changed) setShouldLoad((s) => !s);
+  }, [url, options]);
+
+  useEffect(() => {
+    //the var wait is to the states don't change when the component is already unmounted
+    let wait = false;
     setLoading(true);
 
     const fetchData = async () => {
-      console.log(options);
-      await new Promise((r) => setTimeout(r, 3000));
+      await new Promise((r) => setTimeout(r, 1000));
 
       try {
-        const response = await fetch(url, { method: options });
+        const response = await fetch(urlRef.current, optionsRef.current);
         const jsonResult = await response.json();
-        setResult(jsonResult);
-        setLoading(false);
+
+        if (!wait) {
+          setResult(jsonResult);
+          setLoading(false);
+        }
       } catch (e) {
-        console.log('error');
-        setLoading(false);
+        if (!wait) setLoading(false);
         throw e;
       }
     };
 
     fetchData();
-  }, [url, options]);
+
+    return () => {
+      wait = true;
+    };
+  }, [shouldLoad]);
 
   return [result, loading];
 };
 
 export function App() {
+  const [postId, setPostId] = useState('');
   //if I uncomment the {method: 'GET'}, the useFetch will execute several times causing error.
-  //hmmmmm, if instead of sending an object, just send the strings that will be assigned to it, it works.
-  const [result, loading] = useFetch('https://jsonplaceholder.typicode.com/posts', 'GET');
+  //teacher solved problem using another hook again (useRef).
+  const [result, loading] = useFetch('https://jsonplaceholder.typicode.com/posts/' + postId, { headers: { abc: 1 } });
+
+  const handleClick = (id) => {
+    setPostId(id);
+  };
 
   if (loading) {
-    console.log('loading: true');
     return <h1>Loading...</h1>;
   }
   if (!loading) {
-    console.log('loading: false');
-    console.log(result);
     return (
       <div>
         <h1>Title</h1>
-        {result.map((post) => {
-          return (
-            <div key={post.id}>
-              <h3>{post.title}</h3>
-            </div>
-          );
-        })}
+        {/* here, he used a ternary operator to chose if should print an array or an object. If you click
+        in a title, it will load the object. If we click again, it will load the hole array again. */}
+        {result?.length > 0 ? (
+          result.map((post) => {
+            return (
+              <div key={post.id}>
+                <h3 onClick={() => handleClick(post.id)}>{post.title}</h3>
+              </div>
+            );
+          })
+        ) : (
+          <div key={result.id}>
+            <h3 onClick={() => handleClick('')}>{result.title}</h3>
+          </div>
+        )}
       </div>
     );
   }
