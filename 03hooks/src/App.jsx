7dc51349bdf3f7557d8bcbd6b76ebcc87181bbd8
Sadlark1993 <claudio.zi.junior@gmail.com>
2023-02-02 21:39:12 -0400

    Lesson: forwardRef

--- 03hooks/src/App.jsx
+++ 03hooks/src/App.jsx
@@ -1,5 +1,6 @@
-import { useEffect, useLayoutEffect } from 'react';
+import { forwardRef, useImperativeHandle, useLayoutEffect } from 'react';
 import { useState, useRef } from 'react';
+import PropTypes from 'prop-types';
 
 export const App = () => {
   const [counted, setCounted] = useState([0, 1, 2, 3, 4]);
@@ -8,15 +9,14 @@ export const App = () => {
 
   const handleClick = () => {
     setCounted((c) => [...c, +c.slice(-1) + 1]);
-    divRef.current.scrollTop = divRef.current.scrollHeight;
   };
 
   //with useLayoutEffect, the DOM will be rendered only after all the changes. So, in this case, the page will freeze before be rendered
-  //with useEffect, the page would freeze for 2 seconds while its rendering.
+  //with useEffect, the page would freeze for 300 milliseconds while its rendering.
   //Only use this when you don't have any choice left.
   useLayoutEffect(() => {
     const now = Date.now();
-    while (Date.now() < now + 2000)
+    while (Date.now() < now + 300)
       //freezes the page for 2s
       divRef.current.scrollTop = divRef.current.scrollHeight;
   });
@@ -24,11 +24,22 @@ export const App = () => {
   return (
     <>
       <button onClick={handleClick}>Count {counted.slice(-1)} </button>
-      <div ref={divRef} style={{ height: '200px', width: '200px', overflow: 'scroll' }}>
-        {counted.map((c) => {
-          return <p key={`c-${c}`}>{c}</p>;
-        })}
-      </div>
+      <DisplayContent counted={counted} ref={divRef} />
     </>
   );
+};
+
+//with this, you can save the reference of an element from another react component.
+export const DisplayContent = forwardRef(function DisplayContent({ counted }, ref) {
+  return (
+    <div ref={ref} style={{ height: '200px', width: '200px', overflow: 'scroll' }}>
+      {counted.map((c) => {
+        return <p key={`c-${c}`}>{c}</p>;
+      })}
+    </div>
+  );
+});
+
+DisplayContent.propTypes = {
+  counted: PropTypes.array,
 };
