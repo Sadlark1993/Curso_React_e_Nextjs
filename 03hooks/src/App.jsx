327298a79aeb9503fdaa35db3b90e80be878acc8
Sadlark1993 <claudio.zi.junior@gmail.com>
2023-02-03 21:31:12 -0400

    Lesson: Compound Pattern - 1

--- 03hooks/src/App.jsx
+++ 03hooks/src/App.jsx
@@ -1,26 +1,56 @@
-//inheritance of styles
-import { Children, cloneElement } from 'react';
+//Compound Pattern
+
+import { Children, cloneElement, useState } from 'react';
+import PropTypes from 'prop-types';
 
 const s = {
-  //atributes that will be inserted in the elements.
   style: {
     margin: '10px',
-    fontSize: '1,8em',
+    fontSize: '1.5em',
+    display: 'block',
   },
 };
 
-const Parent = ({ children }) => {
+const TurnOnOff = ({ children }) => {
+  const [itsOn, setItsOn] = useState(false);
+  const onToggle = () => setItsOn((i) => !i);
   return Children.map(children, (child) => {
-    const newChild = cloneElement(child, { ...s, data_number: 25 });
+    const newChild = cloneElement(child, { s, itsOn, onToggle });
     return newChild;
   });
 };
 
+const TurnedOn = ({ itsOn, children, s }) => (itsOn ? <p {...s}> {children} </p> : null);
+TurnedOn.propTypes = {
+  itsOn: PropTypes.bool,
+  children: PropTypes.node,
+  s: PropTypes.object,
+};
+
+const TurnedOff = ({ itsOn, children, s }) => (itsOn ? null : <p {...s}>{children}</p>);
+TurnedOff.propTypes = {
+  itsOn: PropTypes.bool,
+  children: PropTypes.node,
+  s: PropTypes.object,
+};
+
+const ToggleButton = ({ itsOn, onToggle }) => (
+  <button {...s} onClick={onToggle}>
+    Toggle: {itsOn ? 'ON' : 'OFF'}
+  </button>
+);
+ToggleButton.propTypes = {
+  itsOn: PropTypes.bool,
+  children: PropTypes.node,
+  onToggle: PropTypes.func,
+};
+
 export const App = () => {
   return (
-    <Parent>
-      <p>First Element</p>
-      <p>Second Element</p>
-    </Parent>
+    <TurnOnOff>
+      <TurnedOn>This thing shows that it&apos;s turned on</TurnedOn>
+      <TurnedOff>Oh! It&apos;s so dark down here!</TurnedOff>
+      <ToggleButton />
+    </TurnOnOff>
   );
 };
