4a013a247f4ad055b3820c3bc4dc3912172e01e2
Sadlark1993 <claudio.zi.junior@gmail.com>
2023-02-11 14:27:33 -0400

    Lesson: something more about styled components

--- 06landing-pages-react/src/styles/themes.js
+++ 06landing-pages-react/src/styles/themes.js
@@ -2,6 +2,8 @@ export const theme = {
   colors: {
     primaryBg: '#e8fbf6',
     secondaryBg: 'pink',
+    sakuraz: '	#ffb1ef',
+    boom: '#504b78',
   },
   fonts: {},
   spacings: {},
--- 06landing-pages-react/src/templates/App/styles.js
+++ 06landing-pages-react/src/templates/App/styles.js
@@ -4,4 +4,12 @@ export const Wrapper = styled.div`
   ${(props) => css`
     background: ${props.theme.colors.secondaryBg};
   `}
+  color: ${({ theme }) => theme.colors.boom};
+  font-size: 18px;
+  &:hover {
+    color: black;
+  }
+  &::after {
+    content: '----';
+  }
 `;
