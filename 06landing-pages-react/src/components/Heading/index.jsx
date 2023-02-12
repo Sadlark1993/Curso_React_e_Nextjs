054a6970d3637948a96509238c1aa11663225e03
Sadlark1993 <claudio.zi.junior@gmail.com>
2023-02-12 15:52:21 -0400

    Making the tests for the heading component

--- 06landing-pages-react/src/components/Heading/index.jsx
+++ 06landing-pages-react/src/components/Heading/index.jsx
@@ -1,7 +1,7 @@
 import * as Styled from './styles';
 import PropTypes from 'prop-types';
 
-export const Heading = ({ children, darkTitle = true, as = 'h1', size = 'big', uppercase = false }) => {
+export const Heading = ({ children, darkTitle = true, as = 'h1', size = 'huge', uppercase = false }) => {
   return (
     <Styled.titleStyle darkTitle={darkTitle} as={as} size={size} uppercase={uppercase}>
       {children}
@@ -11,7 +11,7 @@ export const Heading = ({ children, darkTitle = true, as = 'h1', size = 'big', u
 
 Heading.propTypes = {
   children: PropTypes.node.isRequired,
-  darkTitle: PropTypes.bool.isRequired,
+  darkTitle: PropTypes.bool,
   as: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']),
   size: PropTypes.oneOf(['small', 'medium', 'big', 'huge']),
   uppercase: PropTypes.bool,
--- 06landing-pages-react/src/templates/App/Home.test.jsx
+++ 06landing-pages-react/src/templates/App/Home.test.jsx
@@ -4,13 +4,8 @@ import Home from '.';
 import { renderTheme } from '../../styles/render-theme';
 import { theme } from '../../styles/themes';
 
-test('testing something', () => {
-  const { debug } = renderTheme(<Home />);
-  debug();
-  const headingContainer = screen.getByRole('heading', { name: 'Hello' }).parentElement;
-
-  expect(headingContainer).toHaveStyle({
-    background: theme.colors.secondaryBg,
+describe('<Home/>', () => {
+  it('should render Home', () => {
+    renderTheme(<Home />);
   });
-  expect(headingContainer).toHaveStyleRule('background', 'pink');
 });
