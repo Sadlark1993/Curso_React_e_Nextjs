9ff0e7d428384caee25824395c8e641dae5d190b
Sadlark1993 <claudio.zi.junior@gmail.com>
2023-02-22 17:16:35 -0400

    Finished GoTop component

---
+++ 06landing-pages-react/src/components/GoTop/GoTop.test.jsx
@@ -0,0 +1,12 @@
+import { renderTheme } from '../../styles/render-theme';
+import { GoTop } from '.';
+import { screen } from '@testing-library/react';
+
+describe('<GoTop />', () => {
+  it('Should render GoTop button', () => {
+    const { container } = renderTheme(<GoTop>Children</GoTop>);
+    expect(screen.getByRole('link', { name: 'Go to top' })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: 'Go to top' })).toHaveAttribute('href', '#');
+    expect(container).toMatchSnapshot();
+  });
+});
--- 06landing-pages-react/src/styles/global-styles.js
+++ 06landing-pages-react/src/styles/global-styles.js
@@ -21,6 +21,7 @@ export const GlobalStyles = createGlobalStyle`
 
   html {
     font-size: 62.5%;
+    scroll-behavior: smooth;
   }
 
   body {
