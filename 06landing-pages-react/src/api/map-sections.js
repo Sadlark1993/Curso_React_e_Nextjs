ae6157367d5955eaa14a869ff53c69513129db5e
Sadlark1993 <claudio.zi.junior@gmail.com>
2023-02-28 00:52:01 -0400

    TDD: maping section-content

--- 06landing-pages-react/src/api/map-sections.js
+++ 06landing-pages-react/src/api/map-sections.js
@@ -43,8 +43,33 @@ export const mapSectionTwoColumns = (section = {}) => {
   return { component, title, text, srcImg, background, sectionId };
 };
 
-export const mapSectionContent = (section) => {
-  return section;
+export const mapSectionContent = (section = {}) => {
+  /*
+  {
+    "__component": "section.section-content",
+    "title": "NEWS COVERAGE AND SOME SURPRISES",
+    "content": "The release of Apple Silicon-based Macs at the end of last year generated a flurry of news coverage and some surprises at the machine’s performance. This post details some background information on the experience of porting Firefox to run natively on these CPUs.\n\nWe’ll start with some background on the Mac transition and give an overview of Firefox internals that needed to know about the new architecture, before moving on to the concept of Universal Binaries.\n\nWe’ll then explain how DRM/EME works on the new platform, talk about our experience with macOS Big Sur, and discuss various updater problems we had to deal with. We’ll conclude with the release and an overview of various other improvements that are in the pipeline.",
+    "metadata": {
+      "section_id": "intro",
+      "background": false
+    }
+  },
+  */
+
+  const {
+    __component: component = 'section.section-content',
+    title = '',
+    content: html = '',
+    metadata: { section_id: sectionId = '', background = false } = '',
+  } = section;
+
+  return {
+    component,
+    title,
+    html,
+    sectionId,
+    background,
+  };
 };
 
 export const mapSectionGrid = (section) => {
--- 06landing-pages-react/src/api/map-sections.test.js
+++ 06landing-pages-react/src/api/map-sections.test.js
@@ -1,4 +1,4 @@
-import { mapSections, mapSectionTwoColumns } from './map-sections';
+import { mapSectionContent, mapSections, mapSectionTwoColumns } from './map-sections';
 
 describe('map-sections', () => {
   it('should return [] when no data is sent', () => {
@@ -57,7 +57,7 @@ describe('map-sections', () => {
         background: true,
       },
     });
-    console.log(data);
+    //console.log(data);
     expect(data.background).toBe(true);
     expect(data.component).toBe('section.section-two-columns');
     expect(data.sectionId).toBe('home');
@@ -66,5 +66,39 @@ describe('map-sections', () => {
       'To wrap up January, we are proud to bring you the release of Firefox 85. In this version we are bringing you support for the :focus-visible pseudo-class in CSS and associated devtools, and the complete removal of Flash support from Firefox.',
     );
     expect(data.title).toBe('JANUARY BRINGS US FIREFOX 85');
+  });
+
+  it('should map section-content when no data is given', () => {
+    const data = mapSectionContent();
+
+    expect(data.background).toBe(false);
+    expect(data.component).toBe('section.section-content');
+    expect(data.html).toBe('');
+    expect(data.sectionId).toBe('');
+    expect(data.title).toBe('');
+  });
+
+  it('should map the given datas to section-content', () => {
+    const data = mapSectionContent({
+      id: 1,
+      __component: 'section.section-content',
+      title: 'NEWS COVERAGE AND SOME SURPRISES',
+      content:
+        'The release of Apple Silicon-based Macs at the end of last year generated a flurry of news coverage and some surprises at the machine’s performance. This post details some background information on the experience of porting Firefox to run natively on these CPUs.\n\nWe’ll start with some background on the Mac transition and give an overview of Firefox internals that needed to know about the new architecture, before moving on to the concept of Universal Binaries.\n\nWe’ll then explain how DRM/EME works on the new platform, talk about our experience with macOS Big Sur, and discuss various updater problems we had to deal with. We’ll conclude with the release and an overview of various other improvements that are in the pipeline.',
+      metadata: {
+        id: 3,
+        name: 'intro',
+        section_id: 'intro',
+        background: false,
+      },
+    });
+
+    expect(data.background).toBe(false);
+    expect(data.component).toBe('section.section-content');
+    expect(data.html).toBe(
+      'The release of Apple Silicon-based Macs at the end of last year generated a flurry of news coverage and some surprises at the machine’s performance. This post details some background information on the experience of porting Firefox to run natively on these CPUs.\n\nWe’ll start with some background on the Mac transition and give an overview of Firefox internals that needed to know about the new architecture, before moving on to the concept of Universal Binaries.\n\nWe’ll then explain how DRM/EME works on the new platform, talk about our experience with macOS Big Sur, and discuss various updater problems we had to deal with. We’ll conclude with the release and an overview of various other improvements that are in the pipeline.',
+    );
+    expect(data.sectionId).toBe('intro');
+    expect(data.title).toBe('NEWS COVERAGE AND SOME SURPRISES');
   });
 });
